072509cded19ec4de7f56e81b8419291aab337cf
Iscle <albertiscle9@gmail.com>
2021-11-21 17:12:02 +0100

    Initial upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const corsAnywhere = require('cors-anywhere')
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 80;
+
+corsAnywhere.createServer({
+    originWhitelist: ['localhost', 'fotos-cocoa-mataro.github.io'],
+}).listen(port, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
